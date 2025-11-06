db6055b238b384cdd84a1336f0999ce62d6ffde0
useong <useong1179@gmail.com>
2025-11-06 21:21:38 +0900

    [SILVER 4] 기타줄

---
+++ baekjoon/1049_기타줄.py
@@ -0,0 +1,18 @@
+# 1049번 기타줄
+
+# 6개 패키지 가격의 min값과 1개 가격 min값을 구해두고,
+# min(6개 패키지만 싹 사
+
+n, m = map(int, input().split())
+min_6, min_1 = 10000, 10000
+
+for i in range(m):
+    price_6, price_1 = map(int, input().split())
+
+    if price_6 < min_6: min_6 = price_6
+    if price_1 < min_1: min_1 = price_1
+
+need_6 = n//6
+if n%6 != 0: need_6 += 1
+
+print(min(n * min_1, n//6 * min_6 + n%6 * min_1, need_6 * min_6))
